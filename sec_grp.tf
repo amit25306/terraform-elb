6aa859205b9c2da2b2e1707de054cc5fbe162553
Amit Mahajan <57314242+amit25306@users.noreply.github.com>
2020-02-16 19:23:54 +0530

    Update sec_grp.tf

--- sec_grp.tf
+++ sec_grp.tf
@@ -10,3 +10,22 @@ resource "aws_security_group" "SG-Devops1" {
 }
 
 
+resource "aws_security_group_rule" "allow-ingress" {
+
+        type            = "ingress"
+        from_port       = "0"
+        to_port         = "0"
+        protocol        = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        security_group_id = "${aws_security_group.SG-Devops1.id}"
+}
+
+resource "aws_security_group_rule" "allow-egress" {
+
+        type            = "egress"
+        from_port       = "0"
+        to_port         = "0"
+        protocol        = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        security_group_id = "${aws_security_group.SG-Devops1.id}"
+}

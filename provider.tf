dc996b62a0bf95229351ea44c4f8ddce5099cb09
Amit Mahajan <57314242+amit25306@users.noreply.github.com>
2020-02-16 17:54:38 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "eu-central-1"
-  access_key = "***********************"
-  secret_key = "***********************"
+  region     = "ap-south-1"
+  access_key = "AKIA5E2ZFAMB5TTNJKHP"
+  secret_key = "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"
 }
